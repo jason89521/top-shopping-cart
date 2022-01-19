94ba1916b4a43e9bb809d62fbb5f3146744c4405
jason89521 <97ssps30212@gmail.com>
2022-01-19 22:53:20 +0800

    Enable the back button in cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { getAllFromCart, removeById } from '../../slices/cartSlice';
 import { Box, List, ListItem } from './Cart.style';
 import deleteIcon from '../../images/delete.png';
 import Input from './Input';
+import backArrow from '../../images/back_arrow.png';
 
 const Cart = () => {
   const cart = useSelector(state => state.cart);
   const prodcuts = getAllFromCart(cart);
 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const renderListItem = () => {
@@ -22,7 +25,11 @@ const Cart = () => {
             <span className="price">${product.price}</span>
             <Input product={product} />
           </div>
-          <img onClick={() => dispatch(removeById(product.id))} src={deleteIcon} alt="delete icon" />
+          <img
+            onClick={() => dispatch(removeById(product.id))}
+            src={deleteIcon}
+            alt="delete icon"
+          />
         </ListItem>
       );
     });
@@ -30,7 +37,10 @@ const Cart = () => {
 
   return (
     <Box>
-      <button>&#8592;</button>
+      <button onClick={() => navigate(-1)}>
+        <img src={backArrow} alt="back" />
+        <span>Back</span>
+      </button>
       <List>{renderListItem()}</List>
     </Box>
   );
--- src/components/Cart/Cart.style.js
+++ src/components/Cart/Cart.style.js
@@ -1,10 +1,39 @@
 import styled from 'styled-components';
 
+import backArrow from '../../images/back_arrow.png';
+
 export const Box = styled.div`
   position: relative;
 
   & > button {
+    cursor: pointer;
     position: absolute;
+    top: 0.5rem;
+    left: 0.5rem;
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    background-color: transparent;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    font-size: 1.5rem;
+    transition: all 0.2s;
+    backface-visibility: hidden;
+
+    &:hover {
+      background-color: rgba(255, 230, 215, 1);
+      transform: translateY(-5px);
+    }
+
+    &:active {
+      transform: translateY(-2px);
+    }
+
+    img {
+      width: 2rem;
+      height: 2rem;
+    }
   }
 `;
 
@@ -13,6 +42,7 @@ export const List = styled.ul`
   width: 70%;
   min-width: 50rem;
   margin: 0 auto;
+  padding: 2rem 0;
 `;
 
 export const ListItem = styled.li`
@@ -21,14 +51,24 @@ export const ListItem = styled.li`
   justify-content: space-between;
   gap: 2rem;
 
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
   img:first-child {
     width: 9rem;
     height: 12rem;
   }
 
   img:last-child {
+    cursor: pointer;
     width: 2rem;
     height: 2rem;
+    transition: all 0.2s;
+
+    &:hover {
+      transform: scale(1.1);
+    }
   }
 
   span.title {
